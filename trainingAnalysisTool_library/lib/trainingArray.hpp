7ef780c02138836dc7e3938e25093d103dbb3f6a
Ranis94 <simonranefjard@gmail.com>
2021-05-11 21:34:09 +0200

    Add some comments for methods

--- trainingAnalysisTool_library/lib/trainingArray.hpp
+++ trainingAnalysisTool_library/lib/trainingArray.hpp
@@ -18,7 +18,7 @@ class TRAININGARRAY
         int m_latestWeek;
         int m_oldestWeek;
 
-        void updateWeeks(int week);
+        void updateWeeks(int week); //Private method to keep track of m_latestWeek and m_oldestWeek of activities in m_trainingInstances
     
     public:
     TRAININGARRAY()
@@ -26,24 +26,26 @@ class TRAININGARRAY
         std::cout << "In trainingArray" << std::endl;
     }
 
+    //Adders
     void addRunningInstance(std::string type, double duration, int week, double cadence, std::map<std::string, double> heartRateZones);
     void addCyclingInstance(std::string type, double duration, int week, double revolutionSpeed);
 
+    //Getters: general
     double getAverageDuration();
     double getTotalDuration();
     double getAverageCadence();
     double getTotalCadence();
-    int getNumberOfActivitiesWithCadence();
     int getNumberOfActivities();
-
     int getLatestWeek();
     int getOldestWeek();
+    double getTotalTimeSpentZone(std::string zone); // This is really
+    int getNumberOfActivitiesWithCadence();
 
-    double getTotalTimeSpentZone(std::string zone);
-
-    // These methods should most likely be usefull to analyse my running
+    //Getters: specified by week and type
     std::map<std::string, double> getWeeklyTimeSpentInZones(int week, std::string type); //Get time spent in each zone for a GIVEN week and activity
     double getWeeklyTime(int week, std::string type); //Get total time for GIVEN week and activity
+
+    //Displayers
     void displayZoneData(std::string type); //Print out W# : Total time: # Zone1: #% ....  , add second input to decide how many weeks to show?
 
 };
