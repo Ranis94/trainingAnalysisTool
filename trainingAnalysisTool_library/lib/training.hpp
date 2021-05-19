4992ffbd05abde610ec068709e5ff003156b13de
Ranis94 <simonranefjard@gmail.com>
2021-05-19 21:32:42 +0200

    Add m_typeEnum to training.hpp

--- trainingAnalysisTool_library/lib/training.hpp
+++ trainingAnalysisTool_library/lib/training.hpp
@@ -13,19 +13,17 @@ class TRAINING
 {
     private:
         double m_duration;
-        std::string m_type = ""; //Enum, then in getType() do dynamic casting to return correct type or not defined. Instanciate as "unclassified"
         int m_week; //Change to date when format is known
         //Add date and time of training as well -> weekday and week number as well. For other purposes later
+        enum m_typeEnum  {running, cycling}; //Add more types as it goes
+        std::string m_type = ""; //Enum, then in getType() do dynamic casting to return correct type or not defined. Instanciate as "unclassified"
 
     public:
         // week is only temporarily added
         TRAINING(double duration, int week)
             : m_duration{duration}, m_week{week}{}
 
-        virtual ~TRAINING() //even virtual destructor makes a class polymorphic!
-        {
-            
-        }
+        virtual ~TRAINING(){} //Virtual destructor makes a class polymorphic!
         
         void setType(); //TBA: Call from within constructor?
         std::string getType(); // do dynamic casting to return correct type or not defined, change m_type
